b528723b8adccab9d798705363fdbb215beb2dd1
bjblank2 <bjblank2@illinois.edu>
2019-10-17 16:59:39 -0500

    deleated atom

--- GUM_mk3/monte_carlo.h
+++ GUM_mk3/monte_carlo.h
@@ -19,13 +19,8 @@ using namespace std;
 void fillRuleList(vector<Rule> &list, const char * rule_file, int offset);
 
 
-void init_calcJK(int site, vector<Atom> &atom_list, vector<Rule> &cluster_rules, vector<Rule> &spin_rules);
-void pair_calcJK(int site, int neighbor, vector<Atom> &atom_list, vector<float> J_K); //////////
-void re_calcJK(int site, int old_home_spin, vector<Atom> &atom_list, vector<Rule> &cluster_rules, vector<Rule> &spin_rules);
-void clacBEGParams(vector<float> &J_K);
-void runMetropolis1(float passes, float temp1, float temp2, float temp_inc, vector<Atom> &atom_list, vector<Rule> &MC_rules);
-float evalCluster(vector<Atom> &atom_list, Cluster &cluster, vector<Rule> &cluster_rules, vector<Rule> &spin_rules, vector<float> &J_K, float temp);
-float evalLattice(float temp, vector<Atom> &atom_list, vector<Rule> &MC_rules);
-float evalSiteEnergy1(float temp, int site, vector<Atom> &atom_list, vector<Rule> &MC_rules);
+void runMetropolis1(float passes, float temp1, float temp2, float temp_inc, SimCell &sim_cell, vector<Rule> &MC_rules);
+float evalLattice(float temp, SimCell &sim_cell, vector<Rule> &MC_rules);
+float evalSiteEnergy1(float temp, int site, SimCell sim_cell, vector<Rule> &MC_rules);
 
 #endif
